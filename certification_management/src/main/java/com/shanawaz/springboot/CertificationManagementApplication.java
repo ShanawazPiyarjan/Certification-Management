c240e9b2f28496b8cd1df018887dec378976dfff
ShanawazPiyarjan <syedshanawaz2206@gmail.com>
2021-11-20 21:46:26 +0530

    Certification Controller added

--- certification_management/src/main/java/com/shanawaz/springboot/CertificationManagementApplication.java
+++ certification_management/src/main/java/com/shanawaz/springboot/CertificationManagementApplication.java
@@ -1,4 +1,4 @@
-package com.shanawaz.springsecurity;
+package com.shanawaz.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
